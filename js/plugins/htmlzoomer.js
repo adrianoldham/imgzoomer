5ae91e427a7d688670f0b077d46b393a0afc4a1f
Edmond Leung <leung.edmond@gmail.com>
2008-08-02 06:19:33 +0000

    Added Html Zoomer plugin

---
+++ js/plugins/htmlzoomer.js
@@ -0,0 +1,34 @@
+Object.extend(ImgZoomer.plugins, {
+    html: {
+        setup: function(zoomer, anchor, zoomedImage) {
+            zoomer.elements = zoomer.elements || [];
+            var element = $(anchor.href.substring(anchor.href.lastIndexOf("#") + 1));
+            
+            if (element) {            
+                var contentDiv = new Element("div");
+                
+                contentDiv.style.position = "absolute";
+                contentDiv.style.top = "0px";
+                contentDiv.style.zIndex = zoomer.options.zIndex - 1;
+                contentDiv.hide();
+                
+                var zoomIndex = zoomer.zoomedImages.index(zoomedImage);
+                zoomer.elements[zoomIndex] = element;
+                
+                return contentDiv;
+            }
+        },
+        
+        setContent: function(zoomer, element, zoomedImage) {
+            var zoomIndex = zoomer.zoomedImages.index(zoomedImage);
+            var htmlElement = zoomer.elements[zoomIndex];
+            
+            if (htmlElement) {
+                element.appendChild(htmlElement);
+                
+                htmlElement.style.overflow = "auto";
+                htmlElement.style.display = "block";            
+            }
+        }
+    }
+});
