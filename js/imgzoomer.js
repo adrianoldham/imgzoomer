63e7e2611864510b621a5dc10c8fcb1b30cf07ed
Edmond Leung <leung.edmond@gmail.com>
2008-08-11 08:33:27 +0000

    Removed remove border code for zoomRect

--- js/imgzoomer.js
+++ js/imgzoomer.js
@@ -494,9 +494,6 @@ ImgZoomer.prototype = {
             contentDiv.style.left = absolutePosition[0] + "px";
             contentDiv.style.top = absolutePosition[1] + "px";
             contentDiv.show();
-
-            if (this.options.zoomRects)
-                this.elements[this.zoomedImages.index(zoomedImage)].style.borderColor = "transparent";
             
             // hide zoomed image if there is a content div overlayed on top
             //zoomedImage.setOpacity(0);
